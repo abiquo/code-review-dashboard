7082180bc70768a87ddd712cfe5b17c03c1e7320
Ignasi Barrera <nacx@apache.org>
2014-10-21 10:27:42 +0200

    Added tutorials repo

--- plugins/abiquo.py
+++ plugins/abiquo.py
@@ -90,4 +90,5 @@ class Abiquo:
                 "https://api.github.com/repos/abiquo/jclouds-plugin",
                 "https://api.github.com/repos/abiquo/platform",
                 "https://api.github.com/repos/abiquo/kairosdb-java-client",
-                "https://api.github.com/repos/abiquo/azure-plugin"]
+                "https://api.github.com/repos/abiquo/azure-plugin",
+                "https://api.github.com/repos/abiquo/tutorials"]
