0c18a251bb251e4cce746c9d5944618996dc4f66
Ignasi Barrera <nacx@apache.org>
2016-01-13 17:13:12 +0100

    Added Jimmy

--- plugins/abiquo.py
+++ plugins/abiquo.py
@@ -95,4 +95,5 @@ class Abiquo:
                 "https://api.github.com/repos/abiquo/collectd-abiquo-cookbook",
                 "https://api.github.com/repos/abiquo/captain-koons",
                 "https://api.github.com/repos/abiquo/rpms",
+                "https://api.github.com/repos/abiquo/jimmy",
                 "https://api.github.com/repos/abiquo/tutorials"]
